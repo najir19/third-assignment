51253ba9e515fccc34f25d73b1b000cc810fd212
najir19 <najirahamed29@gmail.com>
2021-01-23 23:51:17 +0600

    Third assignment done

---
+++ hello.js
@@ -0,0 +1,10 @@
+function double(num){
+    var result = num*2;
+    return result;
+}
+
+var first = double(5);
+var sec = double(50);
+
+var total = first + sec;
+console.log("So total is ",total);
